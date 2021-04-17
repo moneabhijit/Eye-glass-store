55bf4db06c5c7903231c258b48c99ce60a6657ef
Abhijit Mone <mone.abhijit@gmail.com>
2021-04-17 21:47:22 +0530

    header changes

--- frontend/ui/src/component/header/Header.js
+++ frontend/ui/src/component/header/Header.js
@@ -1,13 +1,14 @@
 
 import classes from './Header.module.css';
-function Header() {
+function Header (props) {
     return <div className={classes.header}>
-        <h1 className={classes.head}>Shreeyas Optician</h1>
-        <span className={classes.span}>Home Try On</span>
-        <span className={classes.span}> | Eye-Glasses</span>
-        <span className={classes.span}> | Sunglasses</span>
-        <span className={classes.span}> | Eye-exam</span>
+        <h1 className={classes.head} >Shreeyas Optician</h1>
+        <section> <span className={classes.span} id='Try'>Home Try On</span>
+        <span className={classes.span} id='Eye'> | Eye-Glasses</span>
+        <span className={classes.span} id='Sun'> |  Sunglasses</span>
+        <span className={classes.span} id='Exam'> | Eye-exam</span></section>
     </div>
+    
 }
 
 export default Header
